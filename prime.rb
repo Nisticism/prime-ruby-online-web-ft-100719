e562607ac68be020aa54228bb5b3737d66bca63d
Hans Foster <fosterhans@gmail.com>
2019-10-27 21:22:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
 # Add  code here!
 
 def prime?(number)
-  number.times do |num|
-  if number % num == 0 && num != number && num != 1
-    return FALSE
-  else
-    return TRUE
+  counter = 0
+  while counter < number/2
+    if number % num == 0 && num != number && num != 1
+      return FALSE
+    end
+    counter += 1 
   end
+  TRUE
 end
