4ba3f01ca58f87e882455f72ab58814e3ae3054d
Hans Foster <fosterhans@gmail.com>
2019-10-27 20:55:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 
 def prime?(number)
-  if (number != 2 && number % 2 != 0) && (number != 3 && number % 3 != 0) && 
-    (number != 5 && number % 5 != 0) && (number != 7 && number % 7 != 0) || 
+  if ((number != 2 && number % 2 != 0) && (number != 3 && number % 3 != 0) && 
+    (number != 5 && number % 5 != 0) && (number != 7 && number % 7 != 0)) || 
     (number == 2 || number == 3 || number == 5 || number == 7)
     TRUE
   end
