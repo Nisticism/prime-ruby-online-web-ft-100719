fa231fc56e800cd718a991e5d1b5bda994e2782b
Hans Foster <fosterhans@gmail.com>
2019-10-27 21:03:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@
 def prime?(number)
   if (((number != 2 && number % 2 != 0) && (number != 3 && number % 3 != 0) && 
     (number != 5 && number % 5 != 0) && (number != 7 && number % 7 != 0) && (number != 11 && number % 11 != 0) 
-    && (number != 13 && number % 13 != 0)) || 
+    && (number != 13 && number % 13 != 0))) || 
     (number == 2|| number == 3 || number == 5 || number == 7 || number == 11 || number == 13)) && number > 0 && number != 1
     return TRUE
   end
