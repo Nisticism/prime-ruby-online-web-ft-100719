e946f83aa2291c2f4c2294f40fd94db87752d3b2
Hans Foster <fosterhans@gmail.com>
2019-10-27 20:55:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(number)
   if ((number != 2 && number % 2 != 0) && (number != 3 && number % 3 != 0) && 
     (number != 5 && number % 5 != 0) && (number != 7 && number % 7 != 0)) || 
     (number == 2)
-    TRUE
+    return TRUE
   end
   FALSE
 end
