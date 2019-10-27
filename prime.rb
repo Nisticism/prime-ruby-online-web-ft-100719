55fc78b069a1f317a5e6ff690f9a55883e795b73
Hans Foster <fosterhans@gmail.com>
2019-10-27 21:24:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 
 def prime?(number)
-  counter = 0
-  while counter < number/2
-    if number % num == 0 && num != number && num != 1
+  num = 1
+  while num < number / 2
+    if (number % num == 0 && num != number && num != 1) || number < 0
       return FALSE
     end
-    counter += 1 
+    num += 1 
   end
   TRUE
 end
