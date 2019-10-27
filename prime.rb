d9fc97bb5381e4b5ff31eea7bf29936d98c16cc1
Hans Foster <fosterhans@gmail.com>
2019-10-27 20:46:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,8 @@
 # Add  code here!
 
 def prime?(number)
-  if number%2 != 0 && number%3 != 0 && number%
+  if number%2 != 0 && number%3 != 0 && number%5 != 0 && number%7 != 0
+    TRUE
+  end
+  FALSE
 end
