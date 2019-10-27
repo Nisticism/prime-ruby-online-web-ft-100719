1867fccc219c78441372fe5aea9a709df4caa6f1
Hans Foster <fosterhans@gmail.com>
2019-10-27 21:26:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,13 @@
 def prime?(number)
   num = 1
   while num < number / 2
-    if (number % num == 0 && num != number && num != 1) || number < 0
+    if (number % num == 0 && num != number && num != 1)
       return FALSE
     end
     num += 1 
+  end
+  if number < 0 
+    return FALSE
   end
   TRUE
 end
